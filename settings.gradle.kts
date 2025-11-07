fc9cc895260b1cb76686a101091e60cd8aa5737f
tomer00 <himanshutomer00@gmail.com>
2025-11-07 22:00:04 +0530

    Updated project gradle structure from multiplatform to compose desktop single module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,35 +1,10 @@
-rootProject.name = "SambaBackup"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 pluginManagement {
     repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
+        google()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
+    
 }
+rootProject.name = "SambaBackup"
 
-dependencyResolutionManagement {
-    repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
-    }
-}
-
-//plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-//}
-
-include(":composeApp")
