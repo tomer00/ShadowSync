c7f4f89ad85f7074e2d07c20f9b6db82d1075adf
tomer00 <himanshutomer00@gmail.com>
2026-08-15 21:30:32 +0530

    Main Window

--- composeApp/src/jvmMain/kotlin/com/tomer/backup/ui/MainWindow.kt
+++ composeApp/src/jvmMain/kotlin/com/tomer/backup/ui/MainWindow.kt
@@ -1,24 +1,32 @@
 package com.tomer.backup.ui
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tomer.backup.service.BackupService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun BackupToolUI() {
     val menuItems = listOf(
@@ -57,13 +65,51 @@ fun BackupToolUI() {
                         .clickable {
                             selectedItem = label
                         }
-                        .background(if (selectedItem==label) Color.DarkGray else Color.Transparent)
+                        .background(if (selectedItem == label) Color.DarkGray else Color.Transparent)
                         .padding(12.dp)
                 ) {
                     Text(label, color = Color.White, fontSize = 14.sp)
                 }
                 Spacer(Modifier.height(8.dp))
             }
+            Spacer(Modifier.weight(1f))
+            var hovrerd by remember { mutableStateOf(false) }
+            var backingUP by remember { mutableStateOf(false) }
+            val scale by animateFloatAsState(if (hovrerd) 1.1f else 1f)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .graphicsLayer {
+                        scaleX = scale
+                        scaleY = scale
+                    }
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+                    .clickable {
+                        CoroutineScope(Dispatchers.Default).launch {
+                            backingUP = true
+                            BackupService().startBackup()
+                            backingUP = false
+                        }
+                    }
+                    .onPointerEvent(onEvent = { hovrerd = true }, eventType = PointerEventType.Enter)
+                    .onPointerEvent(onEvent = { hovrerd = false }, eventType = PointerEventType.Exit)
+                    .background(Color.DarkGray)
+                    .padding(12.dp)
+
+            ) {
+                Text("SYNC NOW", color = Color.White, fontSize = 14.sp)
+                if (backingUP) {
+                    Spacer(Modifier.weight(1f))
+                    CircularProgressIndicator(
+                        Modifier.align(Alignment.Top).size(32.dp),
+                        color = Color.White,
+                        strokeWidth = 4.dp,
+                        strokeCap = StrokeCap.Round
+                    )
+                }
+            }
+            Spacer(Modifier.height(8.dp))
         }
 
         // Main Content Area
@@ -73,8 +119,8 @@ fun BackupToolUI() {
                 .background(Color(0xFFF4F4F4))
                 .padding(16.dp)
         ) {
-            when(selectedItem){
-                "Logs"-> LogScreen()
+            when (selectedItem) {
+                "Logs" -> LogScreen()
                 else -> SourcesScreen()
             }
         }
