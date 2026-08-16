40e5747cab9ca0991542b3b77cc4d2c5984489fc
tomer00 <himanshutomer00@gmail.com>
2026-08-16 21:30:47 +0530

    gradel

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,35 @@
+rootProject.name = "SambaBackup"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
         gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-    
 }
-rootProject.name = "SambaBackup"
 
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+//plugins {
+//    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+//}
+
+include(":composeApp")
